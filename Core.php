ad14c181eef0f7a8661273990dc16e52c69bdfcf
philippgithub <mail@philippszyza.com>
2019-01-18 21:59:33 +0100

    Update Core.php

--- Core.php
+++ Core.php
@@ -1,23 +1,44 @@
 <?php
-if(!class_exists("Core")){
-	class Core{
-		const VERSION = "1.0";
-		static $core;
+if (!class_exists("Core")) {
+    class Core
+    {
+        const VERSION = "1.0";
+        private static $core;
 
-		public static function get($name){
-			return self::$core[$name];
-		}
+        /**
+         * Setter
+         * @param string $name
+         * @param mixed $value
+         */
+        public static function set($name, $value)
+        {
+            self::$core[$name] = $value;
+        }
 
-		public static function read($name){
-			return self::$core[$name];
-		}
+        /**
+         * Alias for set
+         */
+        public static function write($name, $value)
+        {
+            self::$core[$name] = $value;
+        }
 
-		public static function set($name, $value){
-			self::$core[$name] = $value;
-		}
+        /**
+         * Getter
+         * @param string $name
+         * @return mixed
+         */
+        public static function get($name)
+        {
+            return self::$core[$name];
+        }
 
-		public static function write($name, $value){
-			self::$core[$name] = $value;
-		}
-	}
+        /**
+         * Alias for get
+         */
+        public static function read($name)
+        {
+            return self::$core[$name];
+        }
+    }
 }
